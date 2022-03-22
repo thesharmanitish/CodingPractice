62aecbbc19e836f18b07ac1da218e87ad62ccf01
nitss007 <nitss007@gmail.com>
2022-03-23 00:28:40 +0530

    more coding

--- src/com/interview/google/greedy/Solution.java
+++ src/com/interview/google/greedy/Solution.java
@@ -14,54 +14,47 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'caesarCipher' function below.
-     *
-     * The function is expected to return a STRING.
-     * The function accepts following parameters:
-     *  1. STRING s
-     *  2. INTEGER k
-     */
+	/*
+	 * Complete the 'caesarCipher' function below.
+	 *
+	 * The function is expected to return a STRING. The function accepts following
+	 * parameters: 1. STRING s 2. INTEGER k
+	 */
 
-    public static String caesarCipher(String s, int k) {
-    // Write your code here
-        StringBuilder sb = new StringBuilder(s.length());
-        int a=25;
-       
+	public static String caesarCipher(String s, int k) {
+		// Write your code here
+		// Write your code here
+		if (s.length() == 0 || k == 0)
+			return s;
+		StringBuilder sb = new StringBuilder(s.length());
+		int a = 26;
+		k = k % 26;
+		for (char ch : s.toCharArray()) {
 
-        for(char ch:s.toCharArray()){
+			if ((ch >= 'A' && ch <= 'Z')) {
+				if (ch + k > 'Z') {
+					int tmp = (int) (ch - a);
+					ch = (char) tmp;
+				}
+				ch += k;
+			} else if (ch >= 'a' && ch <= 'z') {
+				if (ch + k > 'z') {
+					int tmp = (int) (ch - a);
+					ch = (char) tmp;
+				}
+				ch += k;
+			}
 
-            if((ch>='A' && ch<='Z')){
-                ch+=k;  
-                if(ch>'Z'){
-                  int tmp =  (int) (ch-'Z')%a;
-                   char b=(char)tmp;
-                   b+='A';
-                   
-                }
-            }
-            else if(ch>='a' && ch<='z'){
-                ch+=k;
-                if(ch>'z'){
-                   int tmp =  (int) (ch-'z')%a;
-                   char b=(char)tmp;
-                   b+='a';
-                }               
-            }
-            
-            sb.append(ch);
-            
-        }
-        System.out.println(sb);
-        return sb.toString();
+			sb.append(ch);
 
-    }
-
+		}
+		return sb.toString();
+	}
 
 }
 
 public class Solution {
 	public static void main(String[] args) {
-        System.out.println(Result.caesarCipher("Zabcdefghijklmnopqrstuvwxyz-':", 27));
-    }
+		System.out.println(Result.caesarCipher("Zabcdefghijklmnopqrstuvwxyz-':", 27));
+	}
 }
