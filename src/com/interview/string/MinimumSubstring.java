03c66976b8a00819e1a0980a4c203cb7a769897e
nitss007 <nitss007@gmail.com>
2022-04-01 19:32:48 +0530

    more coding

--- src/com/interview/string/MinimumSubstring.java
+++ src/com/interview/string/MinimumSubstring.java
@@ -47,7 +47,73 @@ public class MinimumSubstring {
 		return minLength == Integer.MAX_VALUE ? "" : s.substring(mstart, mstart + minLength);
 	}
 
+	/**
+	 * Max Substring with atmost k Distinct characters
+	 * 
+	 * @param s
+	 * @param k
+	 * @return
+	 */
+	public static int lengthOfLongestSubstringKDistinct(String s, int k) {
+		int[] map = new int[256];
+		int start = 0, end = 0, maxLen = Integer.MIN_VALUE, counter = 0;
+
+		while (end < s.length()) {
+			final char c = s.charAt(end);
+			if (map[c] == 0)
+				counter++;
+			end++;
+			map[c] += 1;
+
+			while (counter > k) {
+
+				final char c2 = s.charAt(start);
+				if (map[c2]-- == 1)
+					counter--;
+
+				start++;
+
+			}
+			maxLen = Math.max(maxLen, end - start);
+		}
+
+		return maxLen;
+	}
+
+	/**
+	 * LongestSubstring - without repeating characters
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public static  int lengthOfLongestSubstring2(String s) {
+		int[] map = new int[128];
+		int start = 0, end = 0, maxLen = 0, counter = 0;
+
+		while (end < s.length()) {
+			final char c1 = s.charAt(end);
+			if (map[c1] > 0)
+				counter++;
+			map[c1]++;
+			end++;
+
+			while (counter > 0) {
+				final char c2 = s.charAt(start);
+				if (map[c2] > 1)
+					counter--;
+				map[c2]--;
+				start++;
+			}
+
+			maxLen = Math.max(maxLen, end - start);
+		}
+
+		return maxLen;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(minWindow("ADOBECODEBANC", "ABC"));
+//		System.out.println(minWindow("ADOBECODEBANC", "ABC"));
+//		System.out.println(lengthOfLongestSubstringKDistinct("ADOBECODEBAANCBE", 4));
+		System.out.println(lengthOfLongestSubstring2("ADOBECODEBAANCBE"));
 	}
 }
