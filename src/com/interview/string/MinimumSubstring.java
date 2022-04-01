e574860a48e9728f4b641d0e45bd65ce68a4fe04
nitss007 <nitss007@gmail.com>
2022-04-01 16:47:35 +0530

    more coding

--- src/com/interview/string/MinimumSubstring.java
+++ src/com/interview/string/MinimumSubstring.java
@@ -1,10 +1,19 @@
 package com.interview.string;
 
-
 /**
  * https://leetcode.com/problems/minimum-window-substring/submissions/
  * 
  * https://thesharmanitish.github.io/til/2022-04-01_minimumwindowsubstring
+ * 
+ * 
+ * Given two strings s and t of lengths m and n respectively, return the minimum
+ * window substring of s such that every character in t (including duplicates)
+ * is included in the window. If there is no such substring, return the empty
+ * string "".
+ * 
+ * The testcases will be generated such that the answer is unique.
+ * 
+ * A substring is a contiguous sequence of characters within the string.
  * 
  * @author nisharma
  *
@@ -12,32 +21,33 @@ package com.interview.string;
 public class MinimumSubstring {
 	public static String minWindow(String s, String t) {
 
-        int[] map = new int[67];
-        for(char c:t.toCharArray())
-            map[c-'A'] +=1; 
-        int start=0, end = 0, counter =t.length(), minLength=Integer.MAX_VALUE, mstart = 0;
-        while(end < s.length()){
-            final char c1 = s.charAt(end);
-            if(map[c1-'A']>0){
-                counter--;
-            }
-            map[c1-'A'] -=1;    
-            end++;
-            while(counter==0){
-                if(minLength>end-start){
-                    minLength =end-start;
-                    mstart = start;
-                }
-                final char c2 = s.charAt(start);
-                map[c2-'A']++;
-                if(map[c2-'A']>0)
-                    counter++;
-                start++;
-            }
-        }
-        return minLength==Integer.MAX_VALUE? "": s.substring(mstart,mstart+minLength);}
-	
+		int[] map = new int[67];
+		for (char c : t.toCharArray())
+			map[c - 'A'] += 1;
+		int start = 0, end = 0, counter = t.length(), minLength = Integer.MAX_VALUE, mstart = 0;
+		while (end < s.length()) {
+			final char c1 = s.charAt(end);
+			if (map[c1 - 'A'] > 0) {
+				counter--;
+			}
+			map[c1 - 'A'] -= 1;
+			end++;
+			while (counter == 0) {
+				if (minLength > end - start) {
+					minLength = end - start;
+					mstart = start;
+				}
+				final char c2 = s.charAt(start);
+				map[c2 - 'A']++;
+				if (map[c2 - 'A'] > 0)
+					counter++;
+				start++;
+			}
+		}
+		return minLength == Integer.MAX_VALUE ? "" : s.substring(mstart, mstart + minLength);
+	}
+
 	public static void main(String[] args) {
-		System.out.println(minWindow( "ADOBECODEBANC","ABC"));
+		System.out.println(minWindow("ADOBECODEBANC", "ABC"));
 	}
 }
